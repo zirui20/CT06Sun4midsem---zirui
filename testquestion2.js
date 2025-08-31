92d6e57b8064fb0a962ad8d43c3bea526e1456a1
zirui20 <firefishpro@gmail.com>
2025-08-31 08:56:08 +0000

    Aug 31, 2025, 8:56 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,6 +2,10 @@ function setup() {
     createCanvas(600,600);
     background(220)
 }
+
+let colorValue = 0;
+let spacing = 90;
+
 function draw() {
     let diameter = 50;
     let numCircles = 10; 
