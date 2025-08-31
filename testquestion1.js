d0021e257caa909125e1d208f7bbb2545d376c1a
zirui20 <firefishpro@gmail.com>
2025-08-31 08:52:44 +0000

    Aug 31, 2025, 8:52 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,17 @@
+function setup() {
+    createCanvas(600,400);
+    background(255);
+}
+
+function draw() {
+    let diameter = 40;
+    circle(150,200,100);
+    triangle(260,200,220,260,280,);
+    quad()
+
+    stroke("red");
+    strokeWeight(10);
+
+
+}
 
