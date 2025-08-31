8f103e23b75d595a2203e68ca1fcff5df402d919
zirui20 <firefishpro@gmail.com>
2025-08-31 09:01:34 +0000

    Aug 31, 2025, 9:01 AM

--- testquestion2.js
+++ testquestion2.js
@@ -10,9 +10,15 @@ function draw() {
     let diameter = 50;
     let numCircles = 10; 
     let totalWidth = diameter * numCircles;
-    for(let i =0; i<10; i++) {
-         let y = startY + i*diameter; 
-    }
+    for(let j =0; j<5; j++) {
+
+        let y = startY + j*diameter; 
+
+        for(let i=0; i<5; i++) {
+            fill(0, i*50, 0); 
+            circle(startX+i*diameter, y, diameter); 
        
 
+}
+    }
 }
