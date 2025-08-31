168b5666b2c019cc620dcbc0b4e5d6a8abc33185
zirui20 <firefishpro@gmail.com>
2025-08-31 08:53:50 +0000

    Aug 31, 2025, 8:53 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,16 +1,4 @@
- let diameter = 50;
+s
+
+let diameter = 50;
     let numCircles = 10; 
-    let totalWidth = diameter * numCircles;
-    let startX = diameter/2; 
-    //let startX = diameter/2;
-    let startY = diameter/2; 
-
-
-    for(let j =0; j<5; j++) {
-
-        let y = startY + j*diameter; 
-
-        for(let i=0; i<5; i++) {
-            fill(0, i*50, 0); 
-            circle(startX+i*diameter, y, diameter); 
-
